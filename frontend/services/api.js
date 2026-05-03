8761d2821e1222da227b498f0a19db4e4e0b82f7
Naziehan Labieb <naziehan06@gmail.com>
2026-05-03 16:17:54 +0700

    deploy backend

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+const baseUrl = process.env.NEXT_PUBLIC_API_URL || "https://compog-overflow.vercel.app";
 
 const request = async (path, options) => {
     const response = await fetch(`${baseUrl}${path}`, options);
